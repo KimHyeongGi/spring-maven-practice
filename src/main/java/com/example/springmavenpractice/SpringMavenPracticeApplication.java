f4281c272cb75b74b83eb2173980d728a14ddeee
khg <yomel4@drmamma.co.kr>
2023-05-29 16:09:17 +0900

    Initial commit

---
+++ src/main/java/com/example/springmavenpractice/SpringMavenPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmavenpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMavenPracticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMavenPracticeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/springmavenpractice/SpringMavenPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmavenpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMavenPracticeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
